c8b63cb65a834a1fc7918fef885a67873b9e5a51
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-07-03 19:10:36 +0500

    added CRUD operations for posts

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from posts.models import Post
+from rest_framework import generics
+from posts.serializers import PostSerializer
+
+# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
