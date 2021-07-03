28509d56a3b43a513dd500b02f13e6aae4a1015a
HeeZJee <hafeezghanchi927@gmail.com>
2021-07-04 00:48:33 +0500

    added authentication at api level

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,14 @@
 from posts.models import Post
-from rest_framework import generics
+from rest_framework import generics, permissions
 from posts.serializers import PostSerializer
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
