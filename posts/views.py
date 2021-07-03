2eb5b834355c1667d54fe26dbc4082f0cca7552f
HeeZJee <hafeezghanchi927@gmail.com>
2021-07-04 01:02:42 +0500

    added authentication at project level

--- posts/views.py
+++ posts/views.py
@@ -4,11 +4,9 @@ from posts.serializers import PostSerializer
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
