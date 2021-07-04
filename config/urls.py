89acfef09078f639f43decdf07801c87578a8bed
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-07-05 03:58:33 +0500

    added openapi for api documentation

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,13 @@ from django import urls
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from rest_framework.schemas import get_schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('openapi', get_schema_view( title="Blog API", description="A sample API for learning DRF", version="1.0.0" ), name='openapi-schema')
 
 ]
